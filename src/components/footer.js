2a505ae34b61815f4ff5fc5e58fb2e360e76f0b3
Nejla El morabet <elmorabet.n@hotmail.com>
2023-08-14 18:59:42 +0200

    ajout de header et footer

---
+++ src/components/footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-logo">
+        <img src="/path/to/your/logo.png" alt="Logo" />
+      </div>
+      <div className="footer-address">
+        <p>123 Rue Fictive, Ville Imaginaire</p>
+        <p>Téléphone : +123 456 789</p>
+      </div>
+      <div className="footer-developed-by">
+        <p>Développé par Nejla</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/components/header.js
+++ src/components/header.js
@@ -1,32 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'; // Remplacez "Switch" par "Routes"
 
-function Header() {
+const Header = () => {
   return (
-    // <header>
-    //   <nav>
-    //       <ul>
-    //         <li>
-    //           <Link to="/Header"> <img src="logooo" alt="logo_fitnessPack"></img> </Link>
-    //         </li>
-    //         <li>
-    //           <Link to="/menu">menu</Link>
-    //         </li>
-    //         {/* Ajoutez d'autres liens ici */}
-    //       </ul>
-
-    //       <h1>boost your <div className='changingTitle'>mindset</div></h1>
-    //     </nav>
-    // </header>
     <header className="header">
-    <div className="nav-bar">
-      <a href="index.html"><div className="groupe-18"></div></a>
-      <div className="icon-feather-menu">
-        
+      <div className="header-logo">
+        <img src="/path/to/your/logo.png" alt="Logo" />
       </div>
-    </div>
-  </header>
+      <h1 className="header-title">Boost Your Mindset</h1>
+    </header>
   );
-}
+};
 
 export default Header;
