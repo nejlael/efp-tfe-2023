7fd02afd7e0ebd38ef7a8a2b9b40090e530d4323
Nejla El morabet <elmorabet.n@hotmail.com>
2023-08-15 02:13:39 +0200

    header, ajout dune animation

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Header = () => {
+  const [title, setTitle] = useState('Mindset');
+  const keywords = ['Mindset', 'Meal', 'Muscles'];
+  const intervalDuration = 2000; // 1 seconde
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const currentIndex = keywords.indexOf(title);
+      const nextIndex = (currentIndex + 1) % keywords.length;
+      setTitle(keywords[nextIndex]);
+    }, intervalDuration);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [title]);
+
   return (
     <header className="header">
       <div className="header-logo">
         <img src="/path/to/your/logo.png" alt="Logo" />
       </div>
-      <h1 className="header-title">Boost Your Mindset</h1>
+      <h1 className="header-title">
+        Boost Your{' '}
+        <span className="colored-text">{title}</span>
+      </h1>
     </header>
   );
 };
